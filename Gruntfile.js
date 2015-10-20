e2815d311eb2acfe77bbdc8244106526f5fd2d15
Dan Hasting <dan@hasting.info>
2015-10-19 23:01:13 -0400

    Integrate with Travis CI

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,8 @@ module.exports = function(grunt) {
             options: {
                 globals: {
                     jQuery: true
-                }
-            }
+                },
+            },
         },
         watch: {
             files: ['<%= jshint.files %>'],
@@ -23,13 +23,41 @@ module.exports = function(grunt) {
             },
             build: {
                 src: 'src/vgallery.js',
-                dest: 'min/vgallery-<%= pkg.version %>.min.js'
-            }
-        }
+                dest: 'build/vgallery-<%= pkg.version %>.min.js'
+            },
+        },
+        copy: {
+            main: {
+                src: 'src/vgallery.js',
+                dest: 'build/vgallery-<%= pkg.version %>.js',
+            },
+        },
+        compress: {
+            main: {
+                options: {
+                    archive: 'vGalleryjs-<%= pkg.version %>.zip'
+                },
+                files: [
+                    {
+                        flatten: true,
+                        expand: true,
+                        src: [
+                            'src/vgallery-<%= pkg.version %>.js',
+                            'build/vgallery-<%= pkg.version %>.min.js',
+                            'LICENSE'
+                        ],
+                        dest: 'vGallery.js',
+                    },
+                ],
+            },
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('package', ['copy:main', 'compress:main']);
 };
